a9bfed12fd834683f705ebf44724c9e6d7157aa0
Ryan Silva. <r.sfariaa89@gmail.com.>
2025-06-20 20:01:30 +0900

    Recuperando o nome dos jogos.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function alterarStatus(id){
+let gameClicado=document.getElementById(`game-${id}`);
+let imagem=gameClicado.querySelector(`.dashboard__item__img`);
+let botao=gameClicado.querySelector(`.dashboard__item__button`);
+let nomeDoJogo=gameClicado.querySelector(`.dashboard__item__name`);
+alert(gameClicado.textContent);
+}
